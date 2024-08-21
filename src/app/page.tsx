5b424193201f40347285264bdf3c84483540fb9a
adheeb2 <adheebanvar2@gmail.com>
2024-08-21 19:23:09 +0530

    created navbar and some text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import Link from "next/link";
 export default function HomePage() {
   return (
-    <main>
-      <h1>Home Page</h1>
-      <h1>Hi</h1>
-      <h1>how are you</h1>
+    <main className="flex flex-row justify-between ">
+      <div className=" bg-yellow-500 h-screen  text-black ">
+        <h1 className="text-8xl flex items-center p-32 max-w-5xl ">
+          Explore the Natural Richness of Idukki Vattavada Honey Museum
+        </h1>
+        <p className=" text-black bg-yellow-500 ">
+          Your Gateway to Authentic Honey, Chocolates, Spices, and Adventures
+        </p>
+      </div>
+      <div className="flex-1 lg:w-1/2 p-4">
+        <img src="assets/images/landing.png" alt="" />
+      </div>
     </main>
   );
 }
---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { Button } from "./button";
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-">
+      <div className=" container mx-auto flex justify-between items-center ">
+        <Link href="/">
+          <h1 className="text-white  ">Vattavada Honey museum</h1>
+        </Link>
+        <div className="hidden xl:flex items-center gap-8"></div>
+        <Nav />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
