e1fa3de6543cbaa616ec8a7885dd137a54fd0ac8
adheeb2 <adheebanvar2@gmail.com>
2024-08-22 01:27:30 +0530

    added picture and some changes in layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
 import Link from "next/link";
+
 export default function HomePage() {
   return (
-    <main className="flex flex-row justify-between ">
-      <div className=" bg-yellow-500 h-screen  text-black ">
-        <h1 className="text-8xl flex items-center p-32 max-w-5xl ">
-          Explore the Natural Richness of Idukki Vattavada Honey Museum
-        </h1>
-        <p className=" text-black bg-yellow-500 ">
-          Your Gateway to Authentic Honey, Chocolates, Spices, and Adventures
-        </p>
-      </div>
-      <div className="flex-1 lg:w-1/2 p-4">
-        <img src="assets/images/landing.png" alt="" />
+    <main className="flex justify-between">
+      <div className="bg-yellow-500 min-h-screen mt-1 text-black flex items-center w-full">
+        <div className="bg-yellow-500 p-32 max-w-6xl">
+          <h1 className="text-7xl font-sans bg-yellow-500 p-2 leading-tight">
+            Explore the Natural Richness of Idukki at Vattavada Honey Museum
+          </h1>
+          <p className="text-2xl text-black bg-yellow-500">
+            Your Gateway to Authentic Honey, Chocolates, Spices, and Adventures
+          </p>
+          <button className="mt-7 bg-orange-400 text-gray-50 px-8 py-1 border rounded-full border-transparent">
+            Know more
+          </button>
+        </div>
+        <div>
+          <img src="/landing.png" alt="Honey Museum" />
+        </div>
       </div>
     </main>
   );
--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -4,7 +4,7 @@ import Nav from "./Nav";
 
 const Header = () => {
   return (
-    <header className="py-8 xl:py-12 text-">
+    <header className="py-8 xl:py-12 ">
       <div className=" container mx-auto flex justify-between items-center ">
         <Link href="/">
           <h1 className="text-white  ">Vattavada Honey museum</h1>
